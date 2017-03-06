fd59ade7ed6a501b29f31d035f1ab6ff14827bba
Ross Crichton <rcrichton87@googlemail.com>
2017-03-06 15:35:06 +0000

    bus has a passengers array which starts empty

--- Bus.java
+++ Bus.java
@@ -1,3 +1,21 @@
-class Bus{
+public class Bus{
   
+  private Person[] passengers;
+
+  public Bus(){
+    this.passengers = new Person[10];
+  }
+
+  public int passengerCount(){
+    
+    int count = 0;
+
+    for (Person person : passengers){
+      if (person != null){
+        count++;
+      }
+    }
+    return count;
+  }
+
 }
--- BusTest.java
+++ BusTest.java
@@ -1,3 +1,18 @@
-class BusTest{
-  
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class BusTest{
+
+Bus bus;
+
+@Before
+public void before(){
+  bus = new Bus();
+}
+
+@Test
+public void passengersStartsEmpty(){
+  assertEquals(0, bus.passengerCount() );
+}
+
 }
